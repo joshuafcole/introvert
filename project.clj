54d071de3254a392a13076eef3509ff9019aad6a
Joshua Cole <joshuafcole@gmail.com>
2014-01-22 23:11:15 -0800

    Fixes name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-introvert "0.1.0-SNAPSHOT"
+(defproject introvert "0.1.0-SNAPSHOT"
   :description "A small utility belt for introspecting into clojurescript."
   :url "http://github.com/joshuafcole/cljs-introvert"
   :dependencies [[org.clojure/clojure "1.5.1"]]
