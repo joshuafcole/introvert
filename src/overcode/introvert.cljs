9a24bf437acf45d4ad22572a9e0da4e09344df01
Joshua Cole <joshuafcole@gmail.com>
2014-01-24 22:44:43 -0800

    Adds full circular ref support

--- src/overcode/introvert.cljs
+++ src/overcode/introvert.cljs
@@ -1,47 +1,56 @@
 (ns overcode.introvert)
 
-(defn in?
-  "true if seq contains elm"
-  [seq elm]
-  (some #(= elm %) seq)
-  )
-
 (def ->js)
 
-(defn visit [obj visited]
+(defn array-append [arr seq]
+  "Inserts all elements from a Seq into an existing JS array."
+  (doseq [v seq]
+         (.push arr v))
+  arr)
+
+(defn visited? [visited obj]
+  "Check if a collection has been visited. If so, return its JS equivalent."
+  (if (contains? @visited obj)
+    (get @visited obj)))
+
+(defn visit!
   "Marks potentially linked objects as visited to handle circular refs."
-  (cond
-   (nil? obj) true
+  ([visited obj out]
+   (swap! visited assoc obj out)
+   out)
 
-   (vector? obj) true
-   (list?   obj) true
-   (seq?    obj) true
-
-   (in? @visited obj) nil
-   :else (swap! visited conj obj)))
+  ([visited obj] (visit! visited obj true)))
 
 (defn seq->js
   "Converts a seq-like value into a JS Array."
   ([obj visited]
-   (if (visit obj visited)
-     (into-array (map #(->js % visited) obj))
-     obj))
-  ([obj] (seq->js obj (atom ()))))
+   (let [was-visited (visited? visited obj)]
+     (if was-visited
+       was-visited
+       (let [out (array)]
+         (visit! visited obj out)
+         (array-append out (map #(->js % visited) obj))))))
+
+
+  ([obj] (seq->js obj (atom {}))))
 
 (defn map->js
   "Converts a map-like value into a JS Object."
   ([obj visited]
-   (if (visit obj visited)
-     (let [out (js-obj)]
-       (doall (map (fn [pair]
-                     (let [
-                           key (->js (first pair)  visited)
-                           val (->js (second pair) visited)]
-                       (aset out key val)))
-                   obj))
-       out)
-     obj))
-  ([obj] (map->js obj (atom ()))))
+   (let [was-visited (visited? visited obj)]
+     (if was-visited
+       was-visited
+       (let [out (js-obj)]
+         (visit! visited obj out)
+         (doall
+          (map
+           #(let [key (->js (first %)  visited)
+                   val (->js (second %) visited)]
+               (aset out key val))
+           obj))
+         out))))
+
+  ([obj] (map->js obj (atom {}))))
 
 (defn ->js
   "Converts a cljs data structure into a close JS approximation."
@@ -51,12 +60,13 @@
      cljs.core/Symbol  (name obj)
      cljs.core/Atom    (->js @obj visited)
 
-     cljs.core/PersistentVector (seq->js obj visited)
-     cljs.core/IndexedSeq       (seq->js obj visited)
-     cljs.core/LazySeq          (seq->js obj visited)
+     cljs.core/IndexedSeq        (seq->js obj visited)
+     cljs.core/LazySeq           (seq->js obj visited)
+     cljs.core/PersistentVector  (seq->js obj visited)
+     cljs.core/PersistentHashSet (seq->js obj visited)
 
-     cljs.core/PersistentHashSet  (seq->js obj visited)
-     cljs.core/PersistentHashMap  (map->js obj visited)
      cljs.core/PersistentArrayMap (map->js obj visited)
+     cljs.core/PersistentHashMap  (map->js obj visited)
      obj))
-  ([obj] (->js obj (atom ()))))
+
+  ([obj] (->js obj (atom {}))))
