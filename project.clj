76fca76a7e55729a883b4cbcc692253f23858d97
Joshua Cole <joshuafcole@gmail.com>
2014-01-24 23:19:31 -0800

    Reverts dep on cljsbuild to latest stable (desired features werent in the snapshot either)

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/joshuafcole/cljs-introvert"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2127"]]
-  :plugins [[lein-cljsbuild "1.0.2-SNAPSHOT"]
+  :plugins [[lein-cljsbuild "1.0.1"]
             [com.cemerick/clojurescript.test "0.2.1"]]
 
   :hooks [leiningen.cljsbuild]
